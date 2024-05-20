663b145d70e3faf486665e8592e38fca814a21a8
mrafing <dwpu110300@gmail.com>
2024-05-20 19:37:28 +0700

    deleted Model, Seeders, And Views

--- resources/views/forms/perorangan.blade.php
+++
@@ -1,8 +0,0 @@
-{{-- resources/views/forms/perorangan.blade.php --}}
-
-<div class="form-group">
-    <label for="nama_perorangan">Nama Perorangan</label>
-    <input type="text" class="form-control" id="nama_perorangan" name="nama_perorangan" required placeholder="Nama Perorangan">
-</div>
-
-<!-- Tambahkan elemen form lainnya sesuai kebutuhan -->
