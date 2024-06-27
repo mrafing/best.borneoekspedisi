a11d9b8c754255bee7b1ca0d0c1e5f4c0c2a4007
mrafing <dwpu110300@gmail.com>
2024-06-27 15:03:28 +0700

    add model tracking and voidmanifest

---
+++ app/Models/VoidManifest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Mutator\GenUid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VoidManifest extends Model
+{
+    use HasFactory, GenUid;
+    protected $table = 'tb_manifest';
+    protected $guarded = ['id'];
+
+    public function pengirim() {
+        return $this->belongsTo(Pengirim::class, 'id_pengirim', 'id');
+    }
+
+    public function penerima() {
+        return $this->belongsTo(Penerima::class, 'id_penerima', 'id');
+    }
+
+    public function barang() {
+        return $this->belongsTo(Barang::class, 'id_barang', 'id');
+    }
+
+    public function ongkir() {
+        return $this->belongsTo(Ongkir::class, 'id_ongkir', 'id');
+    }
+
+}
