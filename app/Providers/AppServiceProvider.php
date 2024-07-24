ce57cd3444576026f1a0417c3a1065f055110102
mrafing <dwpu110300@gmail.com>
2024-07-24 22:02:43 +0700

    add Gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('gw', function(User $user) {
+            return $user->outlet->tipe === 'gw';
+        });
+
+        Gate::define('mitraa', function(User $user) {
+            return $user->outlet->tipe === 'mitra a';
+        });
+
+        Gate::define('mitrab', function(User $user) {
+            return $user->outlet->tipe === 'mitra b';
+        });
+
+        Gate::define('mitrac', function(User $user) {
+            return $user->outlet->tipe === 'mitra c';
+        });
+
+        Gate::define('gm', function(User $user) {
+            return $user->role === 'gm';
+        });
+
+        Gate::define('master', function(User $user) {
+            return $user->role === 'master';
+        });
+
     }
 }
